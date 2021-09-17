5be5b987dc19bf7acaeb26d2357aaae601170b48
Antonio Thomacelli <tonnytg@gmail.com>
2021-09-16 22:41:44 -0300

    feat: start db and api rest

--- main.go
+++ main.go
@@ -1,1 +1,14 @@
-package ecomm
+package main
+
+import (
+	"ecomm/api"
+	"ecomm/database"
+)
+
+func main(){
+	// start database connection
+	database.InitDatabase()
+
+	// start api handler
+	api.StartApi()
+}
