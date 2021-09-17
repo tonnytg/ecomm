e7a1c130f6c4b053fdd5b6f24638f9ac5e16e763
Antonio Thomacelli <tonnytg@gmail.com>
2021-09-17 17:39:38 -0300

    feat: add entity client

--- entity/client.go
+++ entity/client.go
@@ -1,9 +1,72 @@
-package product
+package client
 
-type Product struct {
-	Address   string `json:"address"`
-	FirstName string `json:"first_name"`
-	LastName  string `json:"last_name"`
-	Password  string `json:"password"`
-	Username  string `json:"username"`
+import (
+	"errors"
+	"github.com/google/uuid"
+)
+
+const (
+	DISABLED = "disabled"
+	ENABLED  = "enabled"
+)
+
+type ClientInterface interface {
+	Enable() error
+	Disable() error
+	GetID() uuid.UUID
+	GetName() string
+	GetUsername() string
+}
+
+type Client struct {
+	Address    string    `json:"address"`
+	Email      string    `json:"email"`
+	ValidEmail bool      `json:"valid_email"`
+	FirstName  string    `json:"first_name"`
+	LastName   string    `json:"last_name"`
+	Password   string    `json:"password"`
+	Username   string    `json:"username"`
+	Status     string    `json:"status"`
+	ID         uuid.UUID `json:"user_id"`
+}
+
+func NewClient(firstname, lastname, username string) *Client {
+	client := Client{
+		ID:        uuid.New(),
+		Status:    DISABLED,
+		FirstName: firstname,
+		LastName:  lastname,
+		Username:  username,
+	}
+	return &client
+}
+
+func (c *Client) GetID() uuid.UUID {
+	return c.ID
+}
+
+func (c *Client) GetUsername() string {
+	return c.Username
+}
+
+func (c *Client) GetName() string {
+	return c.Username
+}
+
+func (c *Client) Disable() error {
+	if c.ValidEmail == true {
+		c.Status = DISABLED
+		c.ValidEmail = false
+		return nil
+	}
+	return errors.New("email must be valid to disabled")
+}
+
+func (c *Client) Enable() error {
+	if c.ValidEmail == false {
+		c.Status = ENABLED
+		c.ValidEmail = true
+		return nil
+	}
+	return errors.New("email must be valid to enable")
 }
